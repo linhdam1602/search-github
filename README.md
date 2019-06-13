e2a5bb03cf6d2626282a603046eff4e08de86149
Linh Dam <linh.dm@sutrixsolutions.com>
2019-06-13 11:49:34 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # search-github
+In the project directory, you can run:
+#npm i
+#npm start
+
+BUILD: 
+#npm run build
+#npm run upload
---
+++ build/precache-manifest.723f6a2cf863e6cef9a5f23b420f9190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f965d659bc3bd592f011bdb9f27467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b710652131aba3584e6f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a837799b13ff0ae0fe6d",
+    "url": "/static/css/main.17fd7b87.chunk.css"
+  },
+  {
+    "revision": "b710652131aba3584e6f",
+    "url": "/static/js/2.3a2b8a8a.chunk.js"
+  },
+  {
+    "revision": "a837799b13ff0ae0fe6d",
+    "url": "/static/js/main.823dd55d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
